4db0e30ab7355b711d26660a0977c5392a12ab6a
delphinus <me@delphinus.dev>
2020-02-01 15:21:04 +0900

    Add a feature for denite.nvim

---
+++ rplugin/python3/denite/kind/floaterm.py
@@ -0,0 +1,36 @@
+from denite.kind.base import Base
+from denite.util import Nvim, Candidates, UserContext
+
+PREVIEW_FILENAME = "[denite-floaterm-preview]"
+
+
+class Kind(Base):
+    def __init__(self, vim: Nvim) -> None:
+        super().__init__(vim)
+
+        self.name = "floaterm"
+        self.default_action = "open"
+        self._previewed_bufnr = -1
+
+    def action_open(self, context: UserContext) -> None:
+        bufnr = context["targets"][0]["action__bufnr"]
+        self.vim.call("nvim_call_dict_function", "g:floaterm", "jump", [bufnr])
+
+    def action_preview(self, context: UserContext) -> None:
+        bufnr = context["targets"][0]["action__bufnr"]
+
+        if context["auto_action"] != "preview" and self._previewed_bufnr == bufnr:
+            self.vim.command("pclose!")
+            return
+
+        denite_window = self.vim.current.window
+        self.vim.call("denite#helper#preview_file", context, PREVIEW_FILENAME)
+        self.vim.command("wincmd P")
+        self.vim.current.buffer.options["swapfile"] = False
+        self.vim.current.buffer.options["bufhidden"] = "wipe"
+        self.vim.current.buffer.options["buftype"] = "nofile"
+        self.vim.current.buffer[:] = self.vim.buffers[bufnr][
+            0 : self.vim.options["previewheight"]
+        ]
+        self.vim.current.window = denite_window
+        self._previewed_bufnr = bufnr
---
+++ rplugin/python3/denite/source/floaterm.py
@@ -0,0 +1,66 @@
+from denite.base.source import Base
+from denite.util import Nvim, UserContext, Candidate, Candidates
+
+DELIMITER = "\u00a0:\u00a0"
+FLOATERM_HIGHLIGHT_SYNTAX = [
+    {"name": "Bufnr", "link": "Constant", "re": r"\d\+ ", "next": "Name"},
+    {"name": "Name", "link": "Function", "delimiter": DELIMITER, "next": "Title"},
+    {"name": "Title", "link": "Title", "re": r".*"},
+]
+
+
+class Source(Base):
+    def __init__(self, vim: Nvim) -> None:
+        super().__init__(vim)
+
+        self.name = "floaterm"
+        self.kind = "floaterm"
+
+    def on_init(self, context: UserContext) -> None:
+        if self._can_use_floaterm():
+            self.vim.call("nvim_call_dict_function", "g:floaterm", "hide", [])
+
+    def gather_candidates(self, context: UserContext) -> Candidates:
+        if not self._can_use_floaterm():
+            return []
+
+        def candidate(bufnr: int) -> Candidate:
+            name = self.vim.buffers[bufnr].name
+            title = self.vim.call("nvim_buf_get_var", bufnr, "term_title")
+            return {
+                "word": name,
+                "abbr": f"{bufnr: >2} {DELIMITER}{name}{DELIMITER} {title}",
+                "action__bufnr": bufnr,
+            }
+
+        return [
+            candidate(x)
+            for x in self.vim.call(
+                "nvim_call_dict_function", "g:floaterm", "gather", []
+            )
+        ]
+
+    def highlight(self) -> None:
+        for i, syn in enumerate(FLOATERM_HIGHLIGHT_SYNTAX):
+
+            def syn_name(key: str) -> str:
+                return "_".join([self.syntax_name, syn[key]])
+
+            self.vim.command(f"highlight default link {syn_name('name')} {syn['link']}")
+            containedin = f" containedin={self.syntax_name}" if i == 0 else ""
+            nextgroup = f" nextgroup={syn_name('next')}" if "next" in syn else ""
+            if "delimiter" in syn:
+                self.vim.command(
+                    "syntax region {0} matchgroup=Delimiter start=/{1}/ end=/{1}/ concealends contained{2}{3}".format(
+                        syn_name("name"), syn["delimiter"], containedin, nextgroup
+                    )
+                )
+            else:
+                self.vim.command(
+                    "syntax match {0} /{1}/ contained{2}{3}".format(
+                        syn_name("name"), syn["re"], containedin, nextgroup
+                    )
+                )
+
+    def _can_use_floaterm(self) -> bool:
+        return "floaterm" in self.vim.vars
