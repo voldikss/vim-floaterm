d88d134a91ee725224dc2c26b0cd73d296bec9af
delphinus <me@delphinus.dev>
2020-02-02 16:26:30 +0900

    Use shortcut for calling functions

--- rplugin/python3/denite_floaterm.py
+++ rplugin/python3/denite_floaterm.py
@@ -8,27 +8,25 @@ T = TypeVar("T", bound=Callable[..., Any])
 class Floaterm:
     def __init__(self, vim: Nvim) -> None:
         self.vim = vim
-        self.is_nvim = bool(vim.call("has", "nvim"))
+        self.is_nvim = bool(vim.funcs.has("nvim"))
 
     def can_use(self) -> bool:
         return (
             "floaterm" in self.vim.vars
             if self.is_nvim
-            else bool(self.vim.call("exists", "g:floaterm"))
+            else bool(self.vim.funcs.exists("g:floaterm"))
         )
 
     def term_title(self, bufnr: int) -> str:
         return str(
-            self.vim.call("nvim_buf_get_var", bufnr, "term_title")
+            self.vim.api.buf_get_var(bufnr, "term_title")
             if self.is_nvim
-            else self.vim.call("getbufvar", bufnr, "term_title")
+            else self.vim.funcs.getbufvar(bufnr, "term_title")
         )
 
     def call(self, name: str, *args: List[Any], **kwargs: Dict[str, Any]) -> Any:
         if self.is_nvim:
-            return self.vim.call(
-                "nvim_call_dict_function", "g:floaterm", name, args, **kwargs
-            )
+            return self.vim.api.call_dict_function("g:floaterm", name, args, **kwargs)
 
         args_str = ", ".join([f"'{x}'" if isinstance(x, str) else str(x) for x in args])
         return self.vim.eval(f"g:floaterm.{name}({args_str})", **kwargs)
@@ -46,10 +44,10 @@ class Floaterm:
         if self.is_nvim:
             self._current_window = self.vim.current.window
         else:
-            self._current_window = self.vim.call("win_getid")
+            self._current_window = self.vim.funcs.win_getid()
 
     def _restore_win(self) -> None:
         if self.is_nvim:
             self.vim.current.window = self._current_window
         else:
-            self.vim.call("win_gotoid", self._current_window)
+            self.vim.funcs.win_gotoid(self._current_window)
