dff9177139df153580e29a71906847cbdd7de1d2
voldikss <dyzplus@gmail.com>
2020-06-10 09:44:03 +0800

    Fix tests && add more tests

--- test/run.sh
+++ test/run.sh
@@ -4,3 +4,6 @@ nvim +'Vader test/test_command.vader'
 nvim +'Vader test/test_command_with_bang.vader'
 nvim +'Vader test/test_cmdline.vader'
 nvim +'Vader test/test_floaterm_size.vader'
+
+nvim +'Vader test/test_g_autohide/v_true.vader'
+nvim +'Vader test/test_g_autohide/v_false.vader'
