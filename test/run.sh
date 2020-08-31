54e802702c8856a58a0b56a0f1a5244533d1e936
voldikss <dyzplus@gmail.com>
2020-08-31 18:06:23 +0800

    Finish tests

--- test/run.sh
+++ test/run.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/bash
 # only for local test, not used for github action
-nvim +'Vader test/test_command.vader'
-nvim +'Vader test/test_command_with_bang.vader'
-nvim +'Vader test/test_cmdline.vader'
-nvim +'Vader test/test_floaterm_size.vader'
+nvim +'Vader test/test_command/test_FloatermFirst_Last.vader'
+nvim +'Vader test/test_command/test_FloatermKill.vader'
+nvim +'Vader test/test_command/test_FloatermNew.vader'
+nvim +'Vader test/test_command/test_FloatermPrev_Next.vader'
+nvim +'Vader test/test_command/test_FloatermSend.vader'
+nvim +'Vader test/test_command/test_FloatermShow_Hide.vader'
+nvim +'Vader test/test_command/test_FloatermToggle.vader'
+nvim +'Vader test/test_command/test_FloatermUpdate.vader'
 
 nvim +'Vader test/test_g_autohide/v_true.vader'
 nvim +'Vader test/test_g_autohide/v_false.vader'
